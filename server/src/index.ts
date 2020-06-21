530adb04c62b79ace484951e13b4996c29f39cb6
Odee <odjhey@gmail.com>
2020-06-22 05:51:33 +0800

    prettify server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,62 +1,62 @@
-import { DatabaseManager } from "@accounts/database-manager";
-import { AccountsModule } from "@accounts/graphql-api";
-import MongoDBInterface from "@accounts/mongo";
-import { AccountsPassword } from "@accounts/password";
-import { AccountsServer, ServerHooks } from "@accounts/server";
-import { ApolloServer, makeExecutableSchema } from "apollo-server";
-import gql from "graphql-tag";
-import { mergeResolvers, mergeTypeDefs } from "@graphql-toolkit/schema-merging";
-import mongoose from "mongoose";
+import { DatabaseManager } from '@accounts/database-manager'
+import { AccountsModule } from '@accounts/graphql-api'
+import MongoDBInterface from '@accounts/mongo'
+import { AccountsPassword } from '@accounts/password'
+import { AccountsServer, ServerHooks } from '@accounts/server'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server'
+import gql from 'graphql-tag'
+import { mergeResolvers, mergeTypeDefs } from '@graphql-toolkit/schema-merging'
+import mongoose from 'mongoose'
 
 const start = async () => {
   // Create database connection
   await mongoose.connect(
-    "mongodb://localhost:27017/accounts-js-graphql-example",
+    'mongodb://localhost:27017/accounts-js-graphql-example',
     {
       useNewUrlParser: true,
-    }
-  );
-  const mongoConn = mongoose.connection;
+    },
+  )
+  const mongoConn = mongoose.connection
 
   // Build a storage for storing users
-  const userStorage = new MongoDBInterface(mongoConn);
+  const userStorage = new MongoDBInterface(mongoConn)
   // Create database manager (create user, find users, sessions etc) for accounts-js
   const accountsDb = new DatabaseManager({
     sessionStorage: userStorage,
     userStorage,
-  });
+  })
 
   const accountsPassword = new AccountsPassword({
     // This option is called when a new user create an account
     // Inside we can apply our logic to validate the user fields
     validateNewUser: (user) => {
       // For example we can allow only some kind of emails
-      if (user.email.endsWith(".xyz")) {
-        throw new Error("Invalid email");
+      if (user.email.endsWith('.xyz')) {
+        throw new Error('Invalid email')
       }
 
-      return user;
+      return user
     },
-  });
+  })
 
   // Create accounts server that holds a lower level of all accounts operations
   const accountsServer = new AccountsServer(
-    { db: accountsDb, tokenSecret: "secret", ambiguousErrorMessages: false },
+    { db: accountsDb, tokenSecret: 'secret', ambiguousErrorMessages: false },
     {
       password: accountsPassword,
-    }
-  );
+    },
+  )
 
   accountsServer.on(ServerHooks.ValidateLogin, ({ user }) => {
     // This hook is called every time a user try to login.
     // You can use it to only allow users with verified email to login.
     // If you throw an error here it will be returned to the client.
-  });
+  })
 
   // Creates resolvers, type definitions, and schema directives used by accounts-js
   const accountsGraphQL = AccountsModule.forRoot({
     accountsServer,
-  });
+  })
 
   const typeDefs = gql`
     type PrivateType @auth {
@@ -85,24 +85,24 @@ const start = async () => {
     type Mutation {
       _: String
     }
-  `;
+  `
 
   const resolvers = {
     Query: {
       me: (_, __, ctx) => {
         // ctx.userId will be set if user is logged in
         if (ctx.userId) {
-          return accountsServer.findUserById(ctx.userId);
+          return accountsServer.findUserById(ctx.userId)
         }
-        return null;
+        return null
       },
-      publicField: () => "public",
-      privateField: () => "private",
+      publicField: () => 'public',
+      privateField: () => 'private',
       privateType: () => ({
-        field: () => "private",
+        field: () => 'private',
       }),
     },
-  };
+  }
 
   const schema = makeExecutableSchema({
     typeDefs: mergeTypeDefs([typeDefs, accountsGraphQL.typeDefs]),
@@ -110,17 +110,17 @@ const start = async () => {
     schemaDirectives: {
       ...accountsGraphQL.schemaDirectives,
     },
-  });
+  })
 
   // Create the Apollo Server that takes a schema and configures internal stuff
   const server = new ApolloServer({
     schema,
     context: accountsGraphQL.context,
-  });
+  })
 
   server.listen(4000).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
-};
+    console.log(`🚀  Server ready at ${url}`)
+  })
+}
 
-start();
+start()
